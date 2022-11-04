e980e1c9e620934f521f38a8d79d94011c4d1e41
anasdeyra <anasdoura607@gmail.com>
2022-11-04 11:15:02 +0100

    build fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,3 @@
-// @ts-check
-/* run the build with this set to skip validation */
-!process.env.SKIP_ENV_VALIDATION && (await import("./src/env/server.mjs"));
-
-/**
- * Don't be scared of the generics here.
- * All they do is to give us autocompletion when using this.
- *
- * @template {import('next').NextConfig} T
- * @param {T} config - A generic parameter that flows through to the return type
- * @constraint {{import('next').NextConfig}}
- */
 function defineNextConfig(config) {
   return config;
 }
